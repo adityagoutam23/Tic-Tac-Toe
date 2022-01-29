927309cebcb60010664258cb264044b1e85d4f3f
Aditya Goutam <adityagoutam2318@gmail.com>
2022-01-30 01:40:05 +0530

    The ai is now created which never be defeated in tic tac toe game(impossible level)

--- Tic_Tac_Toe.cpp
+++ Tic_Tac_Toe.cpp
@@ -1,8 +1,14 @@
 #include <iostream>
 #include <stdlib.h>
 #include <time.h>
+#include<cctype>
 
 using namespace std;
+
+struct Move
+{
+    int row, col;
+};
 
 char board[5][11]={
             ' ', ' ', ' ', '|',' ', ' ', ' ', '|', ' ', ' ', ' ',
@@ -53,77 +59,77 @@ void place_X_O(char board[5][11], char X_O){
     switch_place(choice, board, X_O);
 }
 
-bool checker(char board[5][11], int pos, char X_O, char comp){
+bool checker(char board[5][11], int pos){
     switch(pos){
-        case 1: {if(board[0][1]==X_O || board[0][1]==comp){
-                    return false;
+        case 1: {if(board[0][1]==' '){
+                    return true;
                 }
                 else 
-                    return true;
+                    return false;
         }
                 break;
         
-        case 2: {if(board[0][5]==X_O || board[0][5]==comp){
-                    return false;
+        case 2: {if(board[0][5]==' '){
+                    return true;
                 }
                 else 
-                    return true;
+                    return false;
         }
                 break;
 
-        case 3: {if(board[0][9]==X_O || board[0][9]==comp){
-                    return false;
+        case 3: {if(board[0][9]==' '){
+                    return true;
                 }
                 else 
-                    return true;
+                    return false;
         }
                 break;
 
-        case 4: {if(board[2][1]==X_O || board[2][1]==comp){
-                    return false;
+        case 4: {if(board[2][1]==' '){
+                    return true;
                 }
                 else 
-                    return true;
+                    return false;
         }
                 break;
 
-        case 5: {if(board[2][5]==X_O || board[2][5]==comp){
-                    return false;
+        case 5: {if(board[2][5]==' '){
+                    return true;
                 }
                 else 
-                    return true;
+                    return false;
         }
                 break;
 
-        case 6: {if(board[2][9]==X_O || board[2][9]==comp){
-                    return false;
+        case 6: {if(board[2][9]==' '){
+                    return true;
                 }
                 else 
-                    return true;
+                    return false;
         }
                 break;
 
-        case 7: {if(board[4][1]==X_O || board[4][1]==comp){
-                    return false;
+        case 7: {if(board[4][1]==' '){
+                    return true;
                 }
                 else 
-                    return true;
+                    return false;
         }
                 break;
 
-        case 8: {if(board[4][5]==X_O || board[4][5]==comp){
-                    return false;
+        case 8: {if(board[4][5]==' '){
+                    return true;
                 }
                 else 
-                    return true;
+                    return false;
         }
                 break;
 
-        case 9: {if(board[4][9]==X_O || board[4][9]==comp){
-                    return false;
+        case 9: {if(board[4][9]==' '){
+                    return true;
                 }
                 else 
-                    return true;
+                    return false;
         }
                 break;
 
@@ -131,18 +137,19 @@ bool checker(char board[5][11], int pos, char X_O, char comp){
     }
 }
 
-bool winner(char board[5][11]){
-    int flag = 0;
+char winner(char board[5][11]){
+    char X = 'X';
+    char O = 'O';
+    char tie = 'T';
+
     for(int i=0; i<5; i=i+2){
         int j=1;
         if(board[i][j]=='X'&&board[i][j+4]=='X'&&board[i][j+8]=='X'){
-            cout<<"\nX is winner\n";
-            return true;
+            return X;
             break;
         }
         else if(board[i][j]=='O'&&board[i][j+4]=='O'&&board[i][j+8]=='O'){
-            cout<<"\nO is winner\n";
-            return true;
+            return O;
             break;
         }
         else{
@@ -152,13 +159,11 @@ bool winner(char board[5][11]){
     for(int j=1; j<11; j=j+4){
         int i=0;
         if(board[i][j]=='X'&&board[i+2][j]=='X'&&board[i+4][j]=='X'){
-            cout<<"\nX is winner\n";
-            return true;
+            return X;
             break;
         }
         else if(board[i][j]=='O'&&board[i+2][j]=='O'&&board[i+4][j]=='O'){
-            cout<<"\nO is winner\n";
-            return true;
+            return O;
             break;
         }
         else{
@@ -166,43 +171,124 @@ bool winner(char board[5][11]){
         }
     }
     if(board[0][1]=='X'&&board[2][5]=='X'&&board[4][9]=='X'){
-        cout<<"\nX is winner\n";
-        return true;
+        return X;
     }
     if(board[0][1]=='O'&&board[2][5]=='O'&&board[4][9]=='O'){
-        cout<<"\nO is winner\n";
-        return true;
+        return O;
     }
     if(board[0][9]=='X'&&board[2][5]=='X'&&board[4][1]=='X'){
-        cout<<"\nX is winner\n";
-        return true;
+        return X;
     }
     if(board[0][9]=='O'&&board[2][5]=='O'&&board[4][1]=='O'){
-        cout<<"\nO is winner\n";
-        return true;
+        return O;
     }
+    bool checked_place;
+    for(int i=1; i<=9; i++){
+        checked_place = checker(board, i);
+        if(checked_place==false){
+            continue;
+        }
+        else{
+            return 0;
+        }
+    }
+    return tie;
 }
 
-void random_place_X_O(char board[5][11], char X_O, char comp){
+void random_place_X_O(char board[5][11], char X_O, char ai){
     int choice;
     bool checked_value;
     choice = rand() % 9 + 1;
-    checked_value = checker(board, choice, X_O, comp);
+    checked_value = checker(board, choice);
     if(checked_value==true){
-            switch_place(choice, board, comp);
+            switch_place(choice, board, ai);
     }
     if(checked_value==false){
-        random_place_X_O(board, X_O, comp);
+        random_place_X_O(board, X_O, ai);
     }
 }
 
-void level_impossible(char board[5][11]){
-
+int minimax(char board[5][11], int depth, bool isMaximizing, char ai, char player){
+    char result = winner(board);
+    if(ai == 'X'){
+        if(result=='X'){
+        return 1;
+    }
+    if(result=='O'){
+        return -1;
+    }
+    if(result=='T'){
+        return 0;
+    }
+    }
+    else
+    if(result=='X'){
+        return -1;
+    }
+    if(result=='O'){
+        return 1;
+    }
+    if(result=='T'){
+        return 0;
+    }
+    
+    if(isMaximizing){
+        int bestScore = -1000;
+        for(int i = 0; i < 5; i= i+2){
+            for(int j = 1; j <10; j=j+4){
+                if(board[i][j]==' '){
+                    board[i][j] = ai;
+                    int score = minimax(board, depth+1, false, ai, player);
+                    bestScore = max(score, bestScore);
+                    board[i][j] = ' ';
+                }
+            }
+        }
+        return bestScore;
+    }
+    else{
+        int bestScore = 1000;
+        for(int i = 0; i < 5; i= i+2){
+            for(int j = 1; j <10; j=j+4){
+                if(board[i][j]==' '){
+                    board[i][j] = player;
+                    int score = minimax(board, depth+1, true, ai, player);
+                    bestScore = min(score, bestScore);
+                    board[i][j] = ' ';
+                }
+            }
+        }
+        return bestScore;
+    }
 }
 
-int first_turn(char board[5][11], char X_O, char comp, int level){
-    bool check_result;
-    int flag = 0;
+void level_impossible(char board[5][11], char ai, char player){
+    int bestScore = -1000;
+    Move bestMove;
+    bestMove.row = -1;
+    bestMove.col = -1;
+
+    for(int i = 0; i < 5; i= i+2){
+        for(int j = 1; j <10; j=j+4){
+            if(board[i][j]==' '){
+                board[i][j] = ai;
+                int score = minimax(board, 0, false, ai, player);
+                board[i][j] = ' ';
+                if(score > bestScore){
+                    bestScore = score;
+                    bestMove.row = i;
+                    bestMove.col = j;
+                }
+            }
+        }
+    }
+    board[bestMove.row][bestMove.col] = ai;
+    cout<<endl;
+    display_board(board);
+}
+
+int first_turn(char board[5][11], char X_O, char ai, int level){
+    char check_result;
     for(int i=0; i<9 ;i++){
         if(i==0 || i%2==0){
             if(level==1){
@@ -212,41 +298,47 @@ int first_turn(char board[5][11], char X_O, char comp, int level){
                 cout<<endl<<"Player 1 your turn\n";
                 place_X_O(board, X_O);
             }
-            
+            else if(level==3){
+                place_X_O(board, X_O);
+            }
             cout<<endl;
         }
         else{
             if(level==1){
-                random_place_X_O(board, X_O, comp);
+                random_place_X_O(board, X_O, ai);
             }
             else if(level==2){
                 cout<<endl<<"Player 2 your turn\n";
-                place_X_O(board, comp);
+                place_X_O(board, ai);
+            }
+            else if(level==3){
+                level_impossible(board, ai, X_O);
             }
             else cout<<"Something is wrong";
             cout<<endl;
         }
         check_result = winner(board);
-        if(check_result==true){
-            flag = 1;
+        if(check_result=='X'||check_result=='O'||check_result=='T'){
             break;
         }
         else continue;
     }
-    return flag;
+    return check_result;
 }
 
-int second_turn(char board[5][11], char X_O, char comp, int level){
-    bool check_result;
-    int flag =0;
+int second_turn(char board[5][11], char X_O, char ai, int level){
+    char check_result;
     for(int i=0; i<9 ;i++){
         if(i==0 || i%2==0){
             if(level==1){
-                random_place_X_O(board, X_O, comp);
+                random_place_X_O(board, X_O, ai);
             }
             else if(level==2){
                 cout<<endl<<"Player 2 your turn\n";
-                place_X_O(board, comp);
+                place_X_O(board, ai);
+            }
+            else if(level==3){
+                level_impossible(board,ai, X_O);
             }
             cout<<endl;
         }
@@ -258,24 +350,25 @@ int second_turn(char board[5][11], char X_O, char comp, int level){
                 cout<<endl<<"Player 1 your turn\n";
                 place_X_O(board, X_O);
             }
+            else if(level==3){
+                place_X_O(board, X_O);
+            }
             cout<<endl;
         }
         check_result = winner(board);
-        if(check_result==true){
-            flag = 1;
+        if(check_result=='X'||check_result=='O'||check_result=='T'){
             break;
         }
         else continue;
     }
-    return flag;
+    return check_result;
 }
-
 
 int main(){
 
-    int flag;
+    char flag;
     int level;
-    char X_O, comp;
+    char X_O, ai;
     string play_first;
     srand (time(NULL));
     system("clear");
@@ -284,7 +377,7 @@ int main(){
 
     cout<<endl<<"1. Play with computer"<<endl;
     cout<<endl<<"2. Play with friend"<<endl;
-    cout<<endl<<"3. Impossible"<<endl;
+    cout<<endl<<"3. Impossible to defeat"<<endl;
     cout<<endl<<"Please select your mode: ";
     cin>>level;
     switch(level){
@@ -293,24 +386,32 @@ int main(){
                 cin>>X_O;
 
                 if(X_O=='X'){
-                    comp = 'O';
+                    ai = 'O';
                 }
                 if(X_O=='O'){
-                    comp = 'X';
+                    ai = 'X';
                 }
 
                 cout<<"\nDo you want to play first? ";
                 cin>>play_first;
 
                 if(play_first=="y"||play_first=="yes"||play_first=="Y"||play_first=="YES"||play_first=="1"){
-                    flag = first_turn(board, X_O, comp, level);
+                    flag = first_turn(board, X_O, ai, level);
+                    if(flag == 'X')
+                        cout<<"X is WINNER!!!";
+                    else if(flag == 'O')
+                        cout<<"O is WINNER!!!";
+                    else if(flag == 'T')
+                        cout<<"It's a TIE!!!";
                 }
                 else{
-                    flag = second_turn(board,X_O, comp, level);
-                }
-
-                if(flag==0){
-                    cout<<"It's a DRAW\n";
+                    flag = second_turn(board,X_O, ai, level);
+                    if(flag == 'X')
+                        cout<<"X is WINNER!!!";
+                    else if(flag == 'O')
+                        cout<<"O is WINNER!!!";
+                    else if(flag == 'T')
+                        cout<<"It's a TIE!!!";
                 }
         }
         break;
@@ -319,7 +420,7 @@ int main(){
                 cout<<"\nPlayer 1 play as X or O: ";
                 cin>>X_O;
                 cout<<"\nPlayer 2 play as X or O: ";
-                cin>>comp;
+                cin>>ai;
 
                 cout<<"\nWhich player wants to play first? ";
                 cin>>play_first;
@@ -327,21 +428,62 @@ int main(){
                 string temp(1, X_O);
 
                 if(play_first=="first"||play_first=="1"||play_first=="first player"||play_first=="player 1"||play_first==temp){
-                    flag = first_turn(board, X_O, comp, level);
+                    flag = first_turn(board, X_O, ai, level);
+                    if(flag == 'X')
+                        cout<<"X is WINNER!!!";
+                    else if(flag == 'O')
+                        cout<<"O is WINNER!!!";
+                    else if(flag == 'T')
+                        cout<<"It's a TIE!!!";
                 }
                 else{
-                    flag = second_turn(board,X_O, comp, level);
-                }
-
-                if(flag==0){
-                    cout<<"It's a DRAW\n";
+                    flag = second_turn(board,X_O, ai, level);
+                    if(flag == 'X')
+                        cout<<"X is WINNER!!!";
+                    else if(flag == 'O')
+                        cout<<"O is WINNER!!!";
+                    else if(flag == 'T')
+                        cout<<"It's a TIE!!!";
                 }
 
         }
         break;
 
         case 3: {
+                char ai;
+                char checker_value;
+                int flag = 0;
+                cout<<"\nPlay as X or O: ";
+                cin>>X_O;
 
+                if(X_O=='X'){
+                    ai = 'O';
+                }
+                if(X_O=='O'){
+                    ai = 'X';
+                }
+
+                cout<<"Do you wish to play first move? ";
+                cin>>play_first;
+
+                if(play_first=="y"||play_first=="yes"||play_first=="Y"||play_first=="YES"||play_first=="1"){
+                    flag = first_turn(board, X_O, ai, level);
+                    if(flag == 'X')
+                        cout<<"X is WINNER!!!";
+                    else if(flag == 'O')
+                        cout<<"O is WINNER!!!";
+                    else if(flag == 'T')
+                        cout<<"It's a TIE!!!";
+                }
+                else{
+                    flag = second_turn(board,X_O, ai, level);
+                    if(flag == 'X')
+                        cout<<"X is WINNER!!!";
+                    else if(flag == 'O')
+                        cout<<"O is WINNER!!!";
+                    else if(flag == 'T')
+                        cout<<"It's a TIE!!!";
+                }
         }
         break;
 
